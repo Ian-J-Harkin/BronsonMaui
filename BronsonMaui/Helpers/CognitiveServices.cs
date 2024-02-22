90413f471ab4680990a5aa80971c992d5f424e12
Ian-J-Harkin <ijharkin@hotmail.com>
2024-02-22 16:05:09 +0000

    Update CognitiveServices.cs
    remove secret info duh

--- BronsonMaui/Helpers/CognitiveServices.cs
+++ BronsonMaui/Helpers/CognitiveServices.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace CSharpCon22Demo.Helpers
 {            public class CognitiveServicesHelper
         {
-            public string ComputerVisionAPIKey { get; set; } = "782bfd75eccf4a9eb9b5f2a37cb1ab3e";
-            public string ComputerVisionAPIEndpoint { get; set; } = "https://visualresource.cognitiveservices.azure.com/";
+            public string ComputerVisionAPIKey { get; set; } = "*******";
+            public string ComputerVisionAPIEndpoint { get; set; } = "https://xxx.cognitiveservices.azure.com/";
         }
    
 }
